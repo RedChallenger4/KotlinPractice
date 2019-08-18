e247ca9d0fbaec70f5023ad5014012f47fd220f9
Joshua Cadavez <cadav001@umn.edu>
2019-08-18 13:54:14 -0400

    [K-0002] How to change activities with intents

---
+++ app/src/main/java/com/practice/joshua/kotlinpractice/Main2Activity.kt
@@ -0,0 +1,23 @@
+package com.practice.joshua.kotlinpractice
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            val intent = Intent(this, MainActivity::class.java) // class you want to navigate to
+            startActivity(intent)
+        } //test
+    }
+
+}
--- app/src/main/java/com/practice/joshua/kotlinpractice/MainActivity.kt
+++ app/src/main/java/com/practice/joshua/kotlinpractice/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-    }
+    }//test
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
