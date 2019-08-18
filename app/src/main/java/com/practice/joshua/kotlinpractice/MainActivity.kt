d3b2ab1da4419ef25eef79944b127df2d15dbef0
Joshua Cadavez <cadav001@umn.edu>
2019-08-18 14:13:32 -0400

    [K-0003] How to add imageviews and convert pngs to webp or xml drawables

--- app/src/main/java/com/practice/joshua/kotlinpractice/MainActivity.kt
+++ app/src/main/java/com/practice/joshua/kotlinpractice/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.practice.joshua.kotlinpractice
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
@@ -15,25 +16,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-    }//test
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+    private fun viewImagePractice(imageId: Int) {
+        val intent = Intent(this, DetailActivity::class.java) // change to DetailActivity
+        intent.setAction(Intent.ACTION_VIEW)
+        startActivity(intent)
     }
 }
